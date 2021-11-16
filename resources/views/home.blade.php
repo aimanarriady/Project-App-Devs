e68189e8c11e201077f615901fdf3eef49247406
aimanarriady <87179900+aimanarriady@users.noreply.github.com>
2021-11-16 10:49:01 +0800

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    <div class='header-container'>
+            <div class='header'>
+            <div class='time'> Example</div>
+            <div class='logo'><a href="https://lh3.googleusercontent.com/Qp4PF86DrHeZLm7fOZhDcD372TcY522fXGWCo8Reaf1Wg73wVOgreCzZusOV1hy7m9sSlaUkSqw0ZLsgq18kAR6Bpz7fhmLSQdXPUQd1nVvkYNm1dW72wdAQutUhO21XIAiUW5dcxw=w2400?source=screenshot.guru"> <img src="https://lh3.googleusercontent.com/Qp4PF86DrHeZLm7fOZhDcD372TcY522fXGWCo8Reaf1Wg73wVOgreCzZusOV1hy7m9sSlaUkSqw0ZLsgq18kAR6Bpz7fhmLSQdXPUQd1nVvkYNm1dW72wdAQutUhO21XIAiUW5dcxw=w600-h315-p-k" height=150px width=230px/> </a></div>
+            <div class='ex'>Example 2</div>
+            </div>
+        </div>
+
+   <div class='integrate-container'>
+       <div class='dash'>
+           <div class='Manage'>
+               <h2>Manage Patient Profile</h2>
+           </div>
+           <div class='M1'>
+               <h2>Monitor Patient Location</h2>
+           </div>
+           <div class='M1'>
+               <h2>Warning Notification</h2>
+           </div>
+       </div>
+   </div>
+
+   <div class='footer'>
+       <div class='fi footer-one'></div>
+       <div class='fi footer-two'></div>
+       <div class='fi footer-three'></div>
+   </div>
+   @endsection
+</body>
+</html>
